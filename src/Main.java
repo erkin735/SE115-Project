709e6acaca85d238be51604361cf7e813886fe4b
Erkin Sipahi <sipahierkin@gmail.com>
2025-12-13 13:37:52 +0300

    "Implemented bestDayOfMonth method"

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         loadData();
-
-    }
-
-
+        {
+                }
+            }
     public static void loadData() {
         for (int m = 0; m < MONTHS; m++) {
             String fileName = "Data_Files/" + months[m] + ".txt";
@@ -29,10 +28,9 @@ public class Main {
                     int day = Integer.parseInt(p[0].trim()) - 1;
                     String comm = p[1];
                     int profit = Integer.parseInt(p[2].trim());
-
                     int idx = -1;
                     for (int i = 0; i < COMMS; i++) {
-                        if (commodities[i].equals(comm)) {
+                        if (commodities[i].equalsIgnoreCase(comm)) {
                             idx = i;
                             break;
                         }
@@ -57,14 +55,12 @@ public class Main {
                 totals[c] += profitData[m][d][c];
             }
         }
-
         int maxIndex = 0;
         for (int i = 1; i < COMMS; i++) {
             if (totals[i] > totals[maxIndex]) {
                 maxIndex = i;
             }
         }
-
         return commodities[maxIndex];
     }
 
@@ -82,19 +78,17 @@ public class Main {
         return total;
     }
 
-
     public static int commodityProfitInRange(String commodity, int from, int to) {
         if (from < 1 || to > 28 || from > to) return -99999;
 
         int idx = -1;
         for (int i = 0; i < COMMS; i++) {
-            if (commodities[i].equals(commodity)) {
+            if (commodities[i].equalsIgnoreCase(commodity)) {
                 idx = i;
                 break;
             }
         }
         if (idx == -1) return -1;
-
         int total = 0;
         for (int m = 0; m < MONTHS; m++) {
             for (int d = from - 1; d <= to - 1; d++) {
@@ -104,10 +98,23 @@ public class Main {
         return total;
     }
 
-
-
     public static int bestDayOfMonth(int month) {
-        return 1234;
+        if (month < 1 || month > 12) return -1;
+
+        int dayOfBest = 1;
+        int maxProfit = Integer.MIN_VALUE;
+
+        for (int day = 0; day < DAYS; day++) {
+            int totalProfit = 0;
+            for (int comm = 0; comm < COMMS; comm++) {
+                totalProfit += profitData[month - 1][day][comm];
+            }
+            if (totalProfit > maxProfit) {
+                maxProfit = totalProfit;
+                dayOfBest = day + 1;
+            }
+        }
+        return dayOfBest;
     }
 
     public static String bestMonthForCommodity(String comm) {
